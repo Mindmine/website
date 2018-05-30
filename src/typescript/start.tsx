58903207708288db0f406f14e51bc323326e5bd7
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-30 17:24:15 +0300

    add generated docs

--- src/typescript/start.tsx
+++ src/typescript/start.tsx
@@ -1,5 +1,10 @@
 import {h, Component, render} from 'preact'
+import '@mdi/font/css/materialdesignicons.css'
 import 'particles.js'
+
+window['particlesJS'].load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded')
+})
 
 export class App extends Component {
 
@@ -8,13 +13,14 @@ export class App extends Component {
             <div class="fx">
                 <div class="bg">
                     <div class="cog"/>
+                    <div class="rocket"/>
                     <div id="particles-js"/>
                 </div>
                 <div class="content">
                     <div id="logo"/>
                     <p>
                        We specialize in web-based business application development in a wide
-                       range of industries: finance, digital media, e-commerce among others.
+                       range of industries: finance, digital media and ecommerce among others.
                        <br/><br/>
                        Mindmine consists of 4 seasoned IT veterans that combine over 75 years
                        of work experience. Although we are framework and language agnostic our
@@ -22,13 +28,15 @@ export class App extends Component {
                        and React in TypeScript in the front.
                     </p>
                 </div>
+                <ul class="footer">
+                    <li><a href="tel:+358 400 3323 22"><i class="mdi mdi-phone"/>+358 400 3323 22</a></li>
+                    <li><a href="mailto:info@mindmine.fi"><i class="mdi mdi-email"/>info@mindmine.fi</a></li>
+                    <li><i class="mdi mdi-briefcase"/>Y-Tunnus: 222</li>
+                    <li><i class="mdi mdi-copyright"/>2012-2018 Helsinki, Mindmine Oy</li>
+                </ul>
             </div>
         )
     }
 }
 
 render(<App/>, document.body)
-
-window['particlesJS'].load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
