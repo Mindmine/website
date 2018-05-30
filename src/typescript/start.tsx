8e0183a98ada4632123ba9abe6187b8fe0a71f58
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-30 13:50:10 +0300

    add cogs

---
+++ src/typescript/start.tsx
@@ -0,0 +1,11 @@
+import {h, Component, render} from 'preact'
+
+
+export class App extends Component {
+
+    render() {
+        return <div class="cog"/>
+    }
+}
+
+render(<App/>, document.body)
